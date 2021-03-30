b70d78ba474ee090706691226383abf0b51754d4
Gabriel <gquerbes>
2021-03-29 21:14:50 -0400

    some fixes

---
+++ Samples/WidgetSampleCS/Views/SettingsWidget.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Gaming.XboxGameBar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using WidgetSampleCS.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WidgetSampleCS
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SettingsWidget : Page
+    {
+        private XboxGameBarWidget widget = null;
+        public SettingsViewModel ViewModel { get; set; }
+
+        public SettingsWidget()
+        {
+            this.InitializeComponent();
+            ViewModel = new SettingsViewModel();
+            DataContext = ViewModel;
+        }
+
+      
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            widget = e.Parameter as XboxGameBarWidget;
+            widget.RequestedOpacityChanged += Widget_RequestedOpacityChanged;
+
+        }
+
+        private async void Widget_RequestedOpacityChanged(XboxGameBarWidget sender, object args)
+        {
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                SetBackgroundOpacity();
+            });
+        }
+
+        private void SetBackgroundOpacity()
+        {
+            this.Opacity = widget.RequestedOpacity;
+        }
+    }
+}
